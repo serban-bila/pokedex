7d72031fd8730e55e7bb818912cd2b553fc738a6
Serban <serban.bila@gmail.com>
2022-05-22 14:28:03 +0300

    Added source code to master

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 
 import './App.css';
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
-// import PokemonDetails from './routes/pokemon-details/pokemon-details.component';
 
 function App({pokemon}) {
-  // const {name} = pokemon;
 
   return (
     <Routes>
-      <Route path="/pokedex" element={<Navigation/>}>
-        <Route index element={<Home />} />
+      <Route path='/' element={<Navigation/>}>
+        <Route exact index element={<Home />} />
         {/* <Route path="/*" element={<PokemonDetails/>} /> */}
       </Route>
     </Routes>
