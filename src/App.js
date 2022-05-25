13909a90d7270f0f3ce93c63b07d53741a9f2ce2
Serban <serban.bila@gmail.com>
2022-05-25 18:36:43 +0300

    Cleaned-up  the code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 
 function App() {
   return (
       <Routes>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Route path="/" element={<Navigation />}>
           <Route index element={<Home />} />
           <Route path="/:pokemon" element={<Home />} />
           {/* <Route path="/:pokemon" element={<PokemonDetails/>} /> */}
         </Route>
+        </BrowserRouter>
       </Routes>
   );
 };
